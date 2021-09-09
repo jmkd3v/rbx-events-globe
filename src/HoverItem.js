4cdf0165f187484232cca8d7448b105bce162bf7
jmkd3v <jmk@jmksite.dev>
2021-09-09 05:19:32 -0300

    amazing! i'm in pain

--- src/HoverItem.js
+++ src/HoverItem.js
@@ -1,15 +1,29 @@
 function HoverItem(props) {
   return (
-    <div className="font-sans max-w-sm mx-auto bg-white dark:bg-black rounded-xl shadow-md p-4 flex space-x-4 items-center">
-      {(props.data.logo.thumbnail_url && (
+    <div className="font-sans p-4 flex space-x-4 bg-white dark:bg-black rounded-xl shadow-md">
+      {props.data.logo.thumbnail_url && (
         <img
           class="flex-none w-18 h-18 rounded-lg object-cover bg-gray-100"
           width="144"
           height="144"
           src={props.data.logo.thumbnail_url}
         />
-      )) ||
-        ""}
+      )}
+      <div className="min-w-0 relative flex-auto sm:pr-20 lg:pr-0 xl:pr-20 w-auto">
+        <h2 className="text-xl font-semibold text-black dark:text-white mb-0.5">
+          {props.data.title}
+        </h2>
+        <dl className="flex flex-wrap text-sm font-medium whitespace-pre">
+          <div>
+            <dt className="sr-only">City</dt>
+            <dd>{props.data.city}</dd>
+          </div>
+          <div>
+            <dt className="sr-only">Country</dt>
+            <dd>, {props.data.country}</dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
