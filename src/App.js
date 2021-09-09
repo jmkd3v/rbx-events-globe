c1a01fd23e051bdeec525e05f3b5e5222dd8b1e6
jmkd3v <jmk@jmksite.dev>
2021-09-09 03:43:51 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Globe from "react-globe.gl";
+import "./App.css";
 
 function App() {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    // load data
+    fetch("https://events.rbx.com/api/chapter_region?chapters=true")
+      .then((res) => res.json())
+      .then((data) => {
+        data = data[0].chapters;
+        console.log(data);
+        setPlaces(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        labelsData={places}
+        labelLat={(d) => d.latitude}
+        labelLng={(d) => d.longitude}
+        labelText={(d) => d.title}
+        labelSize={0.5}
+        labelDotRadius={0.1}
+        labelColor={() => "rgba(255, 165, 0, 0.75)"}
+        labelResolution={2}
+      />
     </div>
   );
 }
