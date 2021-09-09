c1d6ef0411bb2cb0cd6589e82ecd0259e02d3ab6
jmkd3v <jmk@jmksite.dev>
2021-09-09 04:01:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [places, setPlaces] = useState([]);
+  const [hoverD, setHoverD] = useState();
 
   useEffect(() => {
     // load data
@@ -24,10 +25,19 @@ function App() {
       labelLat={(d) => d.latitude}
       labelLng={(d) => d.longitude}
       labelText={(d) => d.title}
+      labelLabel={(d) => `${d.title}<br>
+      ${d.url}<br>
+      ${d.city}, ${d.country}`}
       labelSize={(d) => 1}
+      onLabelClick={(d) => {
+        window.location.href = d.url;
+      }}
+      onLabelHover={setHoverD}
       labelDotRadius={(d) => 1}
       labelColor={() => "#0099FF"}
-      labelResolution={2}
+      labelResolution={3}
+      labelAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
+      labelsTransitionDuration={300}
     />
   );
 }
